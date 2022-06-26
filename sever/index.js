1c9ebc035d0e348ccf9844d5031c4e61a5bc4aa6
Gary <garylin0969@gmail.com>
2022-06-26 17:00:55 +0800

    0626

---
+++ sever/index.js
@@ -0,0 +1,29 @@
+//創建一個伺服器
+const app = require('express')();
+const http = require('http');
+const cors = require('cors');
+
+app.use(cors());
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    }
+});
+
+//socket連接
+io.on('connection', (socket) => {
+    //連上message(單純名稱) 會有name跟message
+    socket.on('message', ({ name, message }) => {
+        //發出消息時
+        io.emit('message', { name, message });
+    });
+});
+
+//連線
+server.listen(4000, function () {
+    console.log('listening on port 4000');
+});
